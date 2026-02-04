8d88b9c7af4cfb37dedcb6f1a99d37822bd399da
JoshavEngelen <josha@mixnmac.nl>
2026-02-04 10:32:27 +0100

    Added log

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ const { db } = require("../services/database");
 router.get('/', async function(req, res) {
   let users = await db.collection('users').find().toArray();
   console.log(db.databaseName);
+  console.log(users);
   res.json(users);
 });
 
